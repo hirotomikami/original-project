7002a0d946dee3ba77b80954234e46a98bef137e
hirotomikami <hirotomikami@users.noreply.github.com>
2022-12-15 15:36:42 +0900

    firstcommit

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = ({
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID
-});
